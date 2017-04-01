13460156965ca678063ea9c29e0f873ee810942a
niemengfei <nmf209@dudu84.com>
2017-04-01 18:43:00 +0800

    tijiao

--- pages/details/details.js
+++ pages/details/details.js
@@ -1,21 +1,47 @@
 // pages/details/details.js
 Page({
-  data:{},
+  data:{
+    details: {}
+  },
   onLoad:function(options){
+    var _self = this
     // 页面初始化 options为页面跳转所带来的参数
     wx.showLoading({
       title: '加载中',
     })
+    // 动态设置title
     wx.setNavigationBarTitle({
-      title: '当前页面'
+      title: options.title + "《电影《豆瓣"
+    })
+    // 数据请求
+    console.log(options.id)
+    var url = options.id
+    wx.request({
+      url: "https://api.douban.com/v2/movie/subject/" +　options.id,
+      data: {},
+      method: 'POSt', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function(res){
+        _self.setData({
+          details: res.data
+        })
+          console.log(res)
+        // success
+      },
+      fail: function(res) {
+        // fail
+      },
+      complete: function(res) {
+        // complete
+      }
     })
   },
   onReady:function(){
     // 页面渲染完成
+    wx.hideLoading()
   },
   onShow:function(){
-    // 页面显示
-    wx.hideLoading()
+    // 页面显示    
   },
   onHide:function(){
     // 页面隐藏
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,6 @@ Page({
    
   },
   tap: function () {
-    console.log(1)
     wx.switchTab({
       url: '../board/board',
       complete: function (e) {
@@ -14,7 +13,44 @@ Page({
       }
     })
   },
-   onUnload:function(){
+  onLoad: function () {
+    var _self = this
+    // 获取当前定位
+    wx.getLocation({
+      type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+      success: function(res){
+        console.log(res)
+        var lat = res.latitude
+        var lon = res.longitude
+        wx.request({
+          url: 'https://api.map.baidu.com/geocoder/v2/',
+          data: {
+            location: lat + "," + lon,
+            output: "json",
+            ak: "B61195334f65b9e4d02ae75d24fa2c53"
+          },
+          method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+          // header: {}, // 设置请求的 header
+          success: function(res){
+            app.globalData.city = res.data.result.addressComponent.city
+          },
+          fail: function(res) {
+            // fail
+          },
+          complete: function(res) {
+            // complete
+          }
+        })
+      },
+      fail: function(res) {
+        // fail
+      },
+      complete: function(res) {
+        // complete
+      }
+    })
+  },
+  onUnload:function() {
     // 页面关闭
     wx.showLoading({
       title: '加载中',
